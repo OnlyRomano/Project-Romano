7cb9ca9b357992cea81d9bb3153657686b47be9e
Jhun Mark <jhunmarkromano2@gmial.com>
2023-09-07 16:57:00 +0800

    First Commit

---
+++ PrelimActivity2Romano.java
@@ -0,0 +1,12 @@
+public class PrelimActivity2Romano {
+    public static void main(String[] args) {
+        Person Romano = new Person("Jhun Mark M. Romano", 20, 2023, 2002);
+
+        Romano.Age();
+
+        System.out.println("Name: " + Romano.getName());
+        System.out.println("Birth Year: " + Romano.getBirthyear());
+        System.out.println("Year Today: " + Romano.getYeartoday());
+        System.out.println("Age: " + Romano.getage());
+    }
+}
